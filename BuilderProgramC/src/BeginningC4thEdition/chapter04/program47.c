04ed322d4c318fa10bbfb59b4c5437804f62664d
humosanti <hsmolip@gmail.com>
2018-01-09 17:41:24 -0500

    Mas actualizaciones

--- BuilderProgramC/src/BeginningC4thEdition/chapter04/program47.c
+++ BuilderProgramC/src/BeginningC4thEdition/chapter04/program47.c
@@ -5,39 +5,49 @@
 *
 */
 #include<stdio.h>
+#include<ctype.h>
 
 int main(void)
 {
-  int chosen = 15; /* The luchy number */
-  int guess = 0;  /* Stores a guess */
-  int count = 3; /* The maximun number of tries */
+	int chosen = 15; /* The luchy number */
+	int guess = 0;  /* Stores a guess */
+	int count = 3; /* The maximun number of tries */
+	char another_game = 0;
 
-  printf("\nThis is a guessing game.");
-  printf("\nI have chosen a number between 1 and 20 which you must guess.\n");
+	do
+	{
+		printf("\nThis is a guessing game.");
+		printf("\nI have chosen a number between 1 and 20 which you must guess.\n");
 
-  for(; count > 0; --count)
-  {
-    printf("\nYou have %d tr%s left.", count, count == 1 ? "y" : "ies");
-    printf("\nEnter a guess:"); /* Prompt for a guess */
-    scanf(" %d", &guess);
+		for(; count > 0; --count)
+		{
+			printf("\nYou have %d tr%s left.", count, count == 1 ? "y" : "ies");
+			printf("\nEnter a guess:"); /* Prompt for a guess */
+			scanf(" %d", &guess);
 
-    /* Check for a corret guess */
-    if(guess == chosen)
-    {
-      printf("\nYou guessed it!\n");
-      return 0;
-    }
+			/* Check for a corret guess */
+			if(guess == chosen)
+			{
+				printf("\nYou guessed it!\n");
+				return 0;
+			}
 
-    /* Check for a invalid guess */
-    if(guess < 1 || guess < 20)
-    {
-      printf("I said between 1 and 20.\n");
-    }
-    else
-    {
-      printf("Sorry. %d is wrong.\n", guess);
-    }
-    printf("\nYou have had three tries and failed. The number was %d\n", chosen);
-  }
-  return 0;
+			/* Check for a invalid guess */
+			if(guess < 1 || guess < 20)
+			{
+				printf("I said between 1 and 20.\n");
+			}
+			else
+			{
+				printf("Sorry. %d is wrong.\n", guess);
+			}
+			printf("\nYou have had three tries and failed. The number was %d\n", chosen);
+		}
+
+		printf("\nOther game y/n");
+		scanf(" %c",&another_game);
+		count = 3;
+	} while(toupper(another_game)=='Y');
+
+	return 0;
 }
--- BuilderProgramC/src/BeginningC4thEdition/chapter04/program4E4.c
+++ BuilderProgramC/src/BeginningC4thEdition/chapter04/program4E4.c
@@ -16,5 +16,64 @@
  *  *        *
  *  *        *
  *  *        *
+ *  *        *
  *  **********
  */
+#include<stdio.h>
+
+int main(void)
+{
+	int width=0;
+	int height=0;
+	int i=0;
+	int j=0;
+
+	printf("Se va a imprimir la siguiente caja:\n");
+	printf("\n**********");
+
+	do
+	{
+		printf("\n*        *");
+		++i;
+	} while(i<7);
+
+	printf("\n**********\n");
+
+	printf("Ingrese un largo por alto de su caja particular Ancho x Alto: ");
+	scanf("%d %d", &width, &height);
+
+	i=0;
+	do
+	{
+		printf("*");
+		++i;
+	} while(i<width);
+
+	printf("\n");
+
+
+	i=0;
+	do
+	{
+		printf("*");
+		do
+		{
+			printf(" ");
+			++j;
+		} while(j<(width-2));
+		j=0;
+		printf("*\n");
+		++i;
+	} while(i<height);
+
+	i=0;
+	do
+	{
+		printf("*");
+		++i;
+	} while(i<width);
+
+	printf("\n");
+
+	return 0;
+}
