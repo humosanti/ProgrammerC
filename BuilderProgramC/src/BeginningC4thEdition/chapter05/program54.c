e4e4a3ca97fbae04bdfd5ed066afe6c187924583
humosanti <hsmolip@gmail.com>
2018-01-10 15:13:18 -0500

    Update hasta la fecha

---
+++ BuilderProgramC/src/BeginningC4thEdition/chapter05/program54.c
@@ -0,0 +1,36 @@
+/* Program 5.4 Reusing the numbers stored 
+ *
+ * Written by Humberto Molinares
+ * Copyright 2018
+ *
+ *
+ *
+ */
+#include<stdio.h>
+
+int main(void)
+{
+	int numbers[10]; /* Array storing 10 values */
+	int count = 10; /* Number of values to be read */
+	long sum = 0L; /* Sum of the numbers */
+	float average = 0.0f; /* Average of th numbers */
+
+	printf("\nEnter the 10 numbers:\n"); /* Prompt for the input */
+
+	/* Read the ten numbers to be averaged */
+	for(int i=0;i<count;i++)
+	{
+		printf("%2d> ",i+1); /* Read a number */
+		scanf(" %d", &numbers[i]); /* Read a number */
+	       sum += numbers[i]; /* Add it to sum */	
+	}
+
+	average = (float)sum/count; /* Calculate the average */
+
+	for(int i=0;i<count;i++)
+	{
+		printf("\nGrade Number %d was %d",i+1,numbers[i]);
+	}
+	printf("\nAverage of the ten numbers entered is: %f\n",average);
+	return 0;
+}
---
+++ BuilderProgramC/src/BeginningC4thEdition/chapter05/program55.c
@@ -0,0 +1,33 @@
+/* Program 5.5 Using the & operator 
+ *
+ * Written by Humberto Molinares
+ * Copyright 2018
+ *
+ *
+ *
+ */
+#include<stdio.h>
+
+int main(void)
+{
+	/* declare some integer variables */
+	long a = 1L;
+	long b = 2L;
+	long c = 3L;
+
+	/* declare some floating-point variables */
+	double d = 4.0;
+	double e = 5.0;
+	double f = 6.0;
+
+	printf("A variable of type long occupies %ld bytes.", sizeof(long));
+	printf("\nHere are the addresses of some variables of type long:");
+	printf("\nThe address of a is: %p The address of b is: %p",&a,&b);
+	printf("\nThe address of c is: %p",&c);
+	printf("\n\nA variable of type double occupies "
+			"%ld bytes.",sizeof(double));
+	printf("\nHere are the addresses of some variables of type double:");
+	printf("\nThe address of d is: %p The address of e is: %p",&d,&e);
+	printf("\nThe address of f is: %p\n",&f);
+	return 0;
+}
